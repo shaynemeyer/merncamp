b0e34302235ea71a487135f752b2a238256616c8
Shayne Meyer <shayne.meyer@gmail.com>
2021-10-26 19:47:49 -0700

    added Head section and added social network meta tagging

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,34 @@
 import { useContext } from "react";
 import { UserContext } from "../context";
-import ParallaxBG from "../components/cards/ParallaxBG";
+import Head from "next/head";
 import axios from "axios";
+import ParallaxBG from "../components/cards/ParallaxBG";
 import Post from "../components/cards/Post";
 
 function HomePage({ posts }) {
   const [state, setState] = useContext(UserContext);
 
+  const head = () => (
+    <Head>
+      <title>MERNCAMP =&gt; A social network by devs for devs</title>
+      <meta name="description" content="A social network by devs for devs" />
+      <meta
+        property="og:description"
+        content="A social network by devs for devs"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="MERNCAMP" />
+      <meta property="og:url" content="http://merncamp.com" />
+      <meta property="og:url" content="http://merncamp.com" />
+      <meta
+        property="og:image:secure_url"
+        content="http://merncamp.com/images/default.jpeg"
+      />
+    </Head>
+  );
   return (
     <>
+      {head()}
       <ParallaxBG url="/images/default.jpeg" />
       <div className="container">
         <div className="row pt-5">
